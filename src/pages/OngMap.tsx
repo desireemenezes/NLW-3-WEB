45e8cf87a2c8490084922811ef686b1fade64ce2
desireemenezes <deemenezes@hotmail.com>
2020-10-16 21:56:58 -0300

    finish

--- src/pages/OngMap.tsx
+++ src/pages/OngMap.tsx
@@ -1,15 +1,47 @@
-import React from 'react';
-import mapMarkerImg from '../images/map.svg';
+import React, { useEffect, useState }from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi'
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowDownRight, FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import mapFlag from '../images/flag.svg';
+import mapMarkerImg from '../images/map.svg';
+
+import { useHistory } from 'react-router-dom';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
-
 import '../styles/pages/ongmap.css'; 
+import api from '../services/api';
 
+
+interface Ong {
+    id: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+}
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapFlag,
+    iconSize: [55, 55],
+    iconAnchor: [29,68],
+    popupAnchor: [170, 2]
+})
 
 function OngMap () {
+
+    const [ongs, setOngs] = useState<Ong[]>([]);
+
+    useEffect(() => {
+
+        api.get('ongs').then(response => {
+            setOngs(response.data);
+        });
+    }, []) ;// euq uero que execute essa funÃ§Ã£o quando alguma desssas variaveis do vetor mudarem
+
+
+    const { goBack } = useHistory();
+
     return (
       <div id="page-map">
           <aside>
@@ -17,21 +49,45 @@ function OngMap () {
                   <img src={mapMarkerImg} alt="Mappy"></img>
 
                   <h1>Escolha a Ong no Mapa</h1>
-                  <p>Conheça as ações socias, projetos e como colaboar com o crescimento da ong :)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              </p>
+                  <p>ConheÃ§a as aÃ§Ãµes socias, projetos e como colaboar com o crescimento da ong :)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              </p>
           
               </header>
 
               <footer>
                   Porto Alegre, Rs.
+                  <button type="button" onClick={goBack}>
+                    <FiArrowLeft size={24} color="#FFF" />
+                 </button>
+
               </footer>
           </aside>
-          <Map center={[-30.1087957,-51.3172298]} zoom={11} style={{width: '100%', height: '100%'}}>
-              <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Map center={[-30.0295947,-51.2282828]} zoom={15} style={{width: '100%', height: '100%'}}>
+
+              <TileLayer 
+                url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+              />
+
+             {ongs.map(ong => {
+                 console.log(ong);
+                 return (
+                    <Marker key={ong.id} icon={mapIcon} position={[ong.latitude, ong.longitude]} >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        {ong.name}
+                        <Link to={`/ongs/${ong.id}`}>
+                            <FiArrowDownRight size={20} color="#fff"></FiArrowDownRight>
+                        </Link>
+                    </Popup>
+                </Marker>
+                 );
+             })};
+
+            
           </Map>
           
-          <Link to="" className="create-ong">
+          <Link to="/ongs/create" className="create-ong">
               <FiPlus size={32} color="#fff"></FiPlus>
           </Link>
+
       </div>
     );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-
 import Landing from './pages/Landing'
+import Ong from './pages/Ong'
+import CreateOng from './pages/CreateOng'
 import OngMap from './pages/OngMap'
 
 function Routes () {
@@ -10,7 +11,10 @@ function Routes () {
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Landing} />
+
                 <Route path="/app" component={OngMap} />
+                <Route path="/ongs/create" component={CreateOng} />
+                <Route path="/ongs/:id" component={Ong} />
             </Switch>
         </BrowserRouter> 
 
